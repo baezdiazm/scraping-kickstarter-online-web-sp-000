c29397bbd7a20eabe1ff9eee77472a76fa515c8a
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-01-20 20:55:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ def create_project_hash
   # write your code here
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
-      # projects: kickstarter.css("li.project.grid_4")
+       projects: kickstarter.css("li.project.grid_4")
 
   binding.pry
 end
